ab2778f1057896ba13ae64d30bc756912efb09a4
Phillip Truong <TruongHPhillip@gmail.com>
2022-02-05 10:07:57 -0500

    setup selenium and opened site

--- gymBookingBot.py
+++ gymBookingBot.py
@@ -1,1 +1,14 @@
-print('Hello gymbookingbot!')
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = ChromeService(executable_path=ChromeDriverManager().install())
+options = ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get('https://shop.westernmustangs.ca/')
+print(driver.title)
+
+driver.quit()
